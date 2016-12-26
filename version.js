eeff620a792472293d3e94c875eb3299f98cff36
AFZ <mansvejk@mail.ru>
2016-12-26 11:24:33 +0400

    edit vers.html (new background fnd new text web-page) and edit js

--- version.js
+++ version.js
@@ -7,14 +7,22 @@
 	Text+="Изменение 4\n";
 	Text+="Изменение 5\n";
 	Text+="Изменение 6  Отмена 5-й фиксации!\n";
+	Text+="Изменение 7  \n";
 	
 	alert(Text);
+	newFunc_1()
+	newFunc_2()
 }
-
-function newBranch()
+//------------------------------------------------------------
+function newFunc_1()
 {
 	var Text = "Новая функция !!!!"
 	alert(Text);
 }
+//------------------------------------------------------------
+function newFunc_2()
+{
+	var Text = "Новая функция - 2"
+	alert(Text);
+}
 vers();
-newBranch();
