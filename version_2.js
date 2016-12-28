ecaa5176a80bbb924f85924cd112dbff1ccd0b7f
AFZ <mansvejk@mail.ru>
2016-12-28 16:06:44 +0400

    new file new user

---
+++ version_2.js
@@ -0,0 +1,49 @@
+﻿function vers()
+{
+	var Text = ""
+	Text+="Изменение 1\n";
+	Text+="Изменение 2\n";
+	Text+="Изменение 3 Изменено  в основной ветке Изменение - 9\n";
+	Text+="Изменение 4\n";
+	Text+="Изменение 5\n";
+	Text+="Изменение 6  Отмена 5-й фиксации!\n";
+	Text+="Изменение 7  \n";
+	Text+="Изменение 8  \n";
+	Text+="Изменение 10  \n";
+	Text+="Изменение 11 GitHub \n";
+	Text+="Изменение 12 GitHub \n";
+	
+	alert(Text);
+	
+	
+	var resWorkFunc=" ";
+	resWorkFunc+=newFunc_1()
+	resWorkFunc+=newFunc_2()
+	resWorkFunc+=newFunc_3()
+	alert(resWorkFunc)
+	
+}
+//------------------------------------------------------------
+function newFunc_1()
+{
+	var Text = "Новая функция !!!!\n"
+	//alert(Text);
+	return Text;
+}
+//------------------------------------------------------------
+function newFunc_2()
+{
+	var Text = "Новая функция - 2\n"
+	//alert(Text);
+	return Text;
+	
+}
+//------------------------------------------------------------
+function newFunc_3()
+{
+	var Text = "Новая функция - 3\n"
+	//alert(Text);
+	return Text;
+}
+//-------------------------------------------------------------
+vers();
